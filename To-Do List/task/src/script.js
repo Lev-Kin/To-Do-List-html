01eb0498bd339b33df1e2f64104044db65097b2b
Leva <parimalleva@gmail.com>
2022-09-27 11:01:58 +0300

    1 of 4 stages completed

---
+++ To-Do List/task/src/script.js
@@ -0,0 +1,35 @@
+let inputText = document.getElementById('input-task');
+let addTaskButton = document.getElementById('add-task-button');
+let list = document.querySelector('#task-list')
+
+addTaskButton.addEventListener('click', (ev => {
+    if (inputText.value !== "") {
+
+        let newLi = document.createElement('li');
+        list.appendChild(newLi);
+
+        let newInput = document.createElement('input');
+        newInput.innerHTML;
+        newInput.setAttribute('type', 'checkbox');
+        newLi.appendChild(newInput);
+
+        let newSpan = document.createElement('span');
+        newSpan.className = 'task';
+        newSpan.innerHTML = inputText.value;
+        newLi.appendChild(newSpan);
+
+        let newButton = document.createElement('button');
+        newButton.className = 'delete-btn';
+        newButton.innerHTML = 'X';
+        newLi.appendChild(newButton);
+
+        inputText.value = '';
+
+        li = document.getElementsByClassName('delete-btn');
+        for (let i = 0; i < li.length; i++) {
+            li[i].addEventListener('click', function () {
+                li[i].parentElement.remove();
+            })
+        }
+    }
+}));
